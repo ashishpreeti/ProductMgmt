63b4738b997979b70e97fc740dc6c97662dc0c62
ashish patil <patilas@visa.com>
2016-08-12 15:57:06 +0100

    display product image

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -9,6 +9,8 @@ import {Component} from "angular2/src/core/metadata";
 
 export class ProductListComponent {
     pageTitle: string = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
     products: any[] = [
         {
             "productId": 1,
