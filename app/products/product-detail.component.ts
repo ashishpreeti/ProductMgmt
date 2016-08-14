c5be55d11320e77a9baaa5d31209c42f411ae082
ashish patil <patilas@visa.com>
2016-08-14 16:23:53 +0100

    Add a back button on the Product Detail page

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "angular2/src/core/metadata";
 import {RouteParams} from "angular2/src/router/instruction";
+import {Router} from "angular2/src/router/router";
 
 @Component({
     templateUrl: 'app/products/product-detail.component.html'
@@ -7,8 +8,14 @@ import {RouteParams} from "angular2/src/router/instruction";
 export class ProductDetailComponent {
     pageHeading: string = "Product Details";
 
-    constructor(private _routeParams: RouteParams) {
+    constructor(private _routeParams: RouteParams,
+                private _router: Router) {
         console.log("id is "+ _routeParams.get('id'));
+        let id = ":" + _routeParams.get('id');
+        this.pageHeading +=  id;
     }
 
+    onBack(): void {
+        this._router.navigate(['Products']);
+    }
 }
